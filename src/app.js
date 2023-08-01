780369f805c027eb7e61ce49f0380e8a97e5bbca
juanocoronel <138510581+juanocoronel@users.noreply.github.com>
2023-08-01 23:30:29 +0000

    gen.excusas.prework

--- src/app.js
+++ src/app.js
@@ -2,10 +2,30 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["El perro", "Mi abuela", "Mi gato", "Mi hermano"];
+  let action = ["se comió", "ensució", "pisó", "rompió"];
+  let what = ["mi tarea", "las llaves", "el coche", "el cuaderno"];
+  let when = [
+    "antes de ir a clase",
+    "en el peor momento",
+    "cuando no estaba en casa",
+    "mientras almorzaba",
+    "mientras estaba durmiendo"
+  ];
+
+  let valorRandom1 = Math.floor(Math.random() * who.length);
+  let valorRandom2 = Math.floor(Math.random() * action.length);
+  let valorRandom3 = Math.floor(Math.random() * what.length);
+  let valorRandom4 = Math.floor(Math.random() * when.length);
+
+  document.getElementById("excuse").innerHTML =
+    who[valorRandom1] +
+    " " +
+    action[valorRandom2] +
+    " " +
+    what[valorRandom3] +
+    " " +
+    when[valorRandom4];
 };
